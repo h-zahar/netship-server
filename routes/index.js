283c8dbc1b78b696f08293a54c4210a107f507af
h-zahar <hz.rifat@gmail.com>
2023-01-03 15:29:00 +0600

    Included Post Routes to Index

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const authRoutes = require("./auth.route");
 const userRoutes = require("./users.route");
+const postRoutes = require("./posts.route");
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
 
 module.exports = router;
